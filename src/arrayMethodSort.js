7624d76f30195eca4622455ebe451a08f5f0ed9a
Marcos Vinicius <marcos_araujo7@hotmail.com>
2026-02-13 22:56:14 -0300

    add solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,36 +5,54 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
+    if (
+      compareFunction !== undefined &&
+      typeof compareFunction !== 'function'
+    ) {
+      throw new TypeError(
+        'The comparison function must be either a function or undefined',
+      );
+    }
+
     const compare =
       compareFunction ||
-      function (a, b) {
-        const A = String(a);
-        const B = String(b);
+      ((a, b) => {
+        const s1 = String(a);
+        const s2 = String(b);
 
-        if (A > B) {
-          return 1;
-        }
-
-        if (A < B) {
+        if (s1 < s2) {
           return -1;
         }
 
+        if (s1 > s2) {
+          return 1;
+        }
+
         return 0;
-      };
+      });
 
-    for (let i = 1; i < this.length; i++) {
-      const current = this[i];
-      let j = i - 1;
+    const length = this.length;
 
-      while (j >= 0 && compare(this[j], current) > 0) {
-        this[j + 1] = this[j];
-        j--;
+    for (let i = 0; i < length; i++) {
+      let swapped = false;
+
+      for (let j = 0; j < length - 1 - i; j++) {
+        if (compare(this[j], this[j + 1]) > 0) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+          swapped = true;
+        }
       }
 
-      this[j + 1] = current;
+      if (!swapped) {
+        break;
+      }
     }
 
     return this;
+  };
+
+  [].__proto__.sort = function (compareFunction) {
+    return this.sort2(compareFunction);
   };
 }
 
